368fe6371c74804e3971f30b3813309cf08eb52b
usrhere <4880627+usrhere@users.noreply.github.com>
2023-03-04 21:42:49 +0100

    Wireplumber config

--- .config/wireplumber/main.lua.d/51-my-config.lua
+++ .config/wireplumber/main.lua.d/51-my-config.lua
@@ -27,7 +27,7 @@ table.insert(alsa_monitor.rules, rule)
 rule = {
   matches = {
     {
-      { "node.name", "equals", "alsa_output.usb-R__DE_Microphones_R__DE_NT-USB_Mini_E0E7C71C-00.analog-stereo" },
+      { "node.name", "matches", "alsa_output.usb-R__DE_Microphones_R__DE_NT-USB_Mini_E0E7C71C-00.analog-stereo*" },
     },
   },
   apply_properties = {
